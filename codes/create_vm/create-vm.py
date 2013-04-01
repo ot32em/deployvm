e28e98261ae5a9729b2d3db258444c01c8015d75
OT Chen <ot32em@gmail.com>
2013-04-01 19:35:00 +0000

    fix create_gentoo.py download twice

--- codes/create_vm/create-vm.py
+++ codes/create_vm/create-vm.py
@@ -61,15 +61,41 @@ def get_vm_dir(username, vmname, os):
 
 def mkdir(dirname):
     cmd='mkdir -p ' + dirname
-    run_cmd( cmd )
+    return run_cmd(cmd) == 0
 
 def download(uri, save_as):
-    cmd = 'wget %s -O %s' % ( uri, save_as)
-    run_cmd(cmd)
+    cmd = 'wget -N %s -O %s' % ( uri, save_as) # -N for overrite existed files
+    return run_cmd(cmd) == 0
 
 def resize_image( image_path, GB ) :
     cmd = 'resize2fs %s %sG' % (image_path, GB)
-    run_cmd( cmd )
+    return run_cmd( cmd ) == 0
+
+
+def remove_file( file_path ):
+    if os_module.path.exists( file_path):
+        print("Remove file: " + file_path)
+        os_module.remove( file_path )
+        return True
+    return False
+    
+def backup_file( file_path ):
+    if not os_module.path.exists(file_path): 
+        return False
+    append_id = 1
+    dest_path = ''
+    while True:
+        dest_path = file_path + "." + str(append_id)
+        if os_module.path.exists(dest_path):
+            append_id = append_id + 1
+            if append_id > 10 :
+                return False
+            else:
+                continue
+            
+        break
+    cmd = "cp %s %s" % (file_path, dest_path)
+    run_cmd(cmd)
 
 def boot_vm( xml_path ):
     cmd = 'virsh create ' + xml_path
@@ -80,7 +106,7 @@ def addportmapping( vm_name, port ):
     run_cmd( cmd )
 
 def run_cmd(cmd):
-    print('run command: ' + cmd)
+    print('[%s] prepare to run command: %s ' % (time.strftime('%H:%I:%S'), cmd) )
     return os_module.system(cmd)
 
 
@@ -109,38 +135,83 @@ def check_vm_touchable(vm_name):
 def create_gentoo(username, gn, sn, config):
 
     vm_name = get_vm_name( username, gn, sn)
+    vm_dir = get_vm_dir(username, vm_name, VM_OS_UBUNTU)
+    kernel_name = "gentoo-kernel.img"
+    image_name = "gentoo-prototype.img"
+    xmlfile_name = vm_name + ".xml"
 
     if check_vm_alive( vm_name ) :
-        raise SystemExit('VM %s has been created!!' % vm_name)
+        print("Error: VM %s is runninghas been booted.")
+        return False
 
-    vm_dir = get_vm_dir(username, vm_name, VM_OS_UBUNTU)
-
+    ''' pre booting scripts '''
     # make vm directory
     mkdir( vm_dir )
 
-    # deploy a image
-    ''' download kernel '''
-    kernel_uri = repository_url + 'kernel/gentoo-kernel-domu.img'
-    kernel_path = vm_dir + 'gentoo-kernel-domu.img'
-    download( kernel_uri, kernel_path )
+    try:
+        kernel_path = prepare_kernel(vm_dir, kernel_name)
+        image_path = prepare_image(vm_dir, config['disk'], image_name)
 
+        # generate xml descriptor
+        xmlfile_path = vm_dir + xmlfile_name
+        mlc.make_xen_libvirt_config( xmlfile_path, vm_name, config['cpu'], config['memory'], image_path, kernel_path )
+
+        ''' booting scripts '''
+        # create vm by xml file and check alive
+        boot_vm( xmlfile_path )
+        check_vm(vm_name)
+
+        # completed!
+        print('VM %s is up.' % vm_name)
+
+        ''' post booting scritpts '''
+        # configure portmapping with port=22 
+        addportmapping(vm_name, 22)
+        return True
+
+    except Exception as e:
+        print(e)
+        print("Booting Failed... prepare to clean files")
+        
+        clean_kernel_file( vm_dir + kernel_name)
+        clean_image_file( vm_dir + image_name)
+        clean_config_file( vm_dir + xmlfile_name )
+
+
+''' sub functions '''
+def prepare_kernel(vm_dir, kernel_name):
+    ''' download kernel '''
+    kernel_uri = repository_url + 'kernel/' + kernel_name
+    kernel_path = vm_dir + kernel_name
+    if download( kernel_uri, kernel_path ) == False :
+        raise Exception("Download kernel %s failed..." % kernel_uri)
+
+    return kernel_path
+
+def prepare_image(vm_dir, disk_size_gb, image_name):
     ''' download image '''
-    image_uri = repository_url + 'prototype/gentoo-prototype.img'
-    image_path = vm_dir + 'gentoo.img'
-    download(image_uri, image_path)
+    image_uri = repository_url + 'prototype/' + image_name
+    image_path = vm_dir + image_name # image_name might be xxxx-prototype.img
+    if download( image_uri, image_path ) == False :
+        raise Exception("Download image %s failed..." % img_uri)
 
     ''' resize image '''
-    numGBs = config['disk']
-    resize_image( image_path, numGBs)
+    if resize_image( image_path, disk_size_gb) == False :
+        print("Resize fail... Use original size of prototype image.")
+    return image_path
 
-    # generate xml descriptor
-    xml_path = vm_dir + vm_name + '.xml'
-    mlc.make_xen_libvirt_config( xml_path, vm_name, config['cpu'], config['memory'], image_path, kernel_path )
 
-    # create vm by xml file
-    boot_vm( xml_path )
-    
-    # check vm whether boot successfully or not
+def clean_kernel_file( kernel_path ):
+    remove_file( kernel_path )
+
+def clean_image_file( image_path ):
+    remove_file( image_path )
+
+def clean_config_file( xml_path ):
+    backup_file( xml_path )
+
+
+def check_vm(vm_name):
     times = 3
     while True:
         alive = check_vm_alive( vm_name )
@@ -157,16 +228,11 @@ def create_gentoo(username, gn, sn, config):
         if times <= 0 :
             raise SystemExit('VM booting failed...')
 
-    print('VM %s is up.' % vm_name)
-
-    # configure portmapping with port=22 
-    addportmapping(vm_name, 22)
-
 
 if __name__ == '__main__' :
     username = 'ot32em'
 #    vm_name = 'ot32em-1-1'
     config = { 'cpu':'1', 'memory':'512', 'disk':'8' }
-    for i in range(1,4,1):
+    for i in range(1,2,1):
         create_gentoo( username, 1, i, config ) 
     
